6b606d83adc681141c751fe3edd4332d6415151f
theo <theophilus96@gmail.com>
2021-06-01 20:57:24 +0800

    Update About.js

--- ehm/src/components/About.js
+++ ehm/src/components/About.js
@@ -24,138 +24,25 @@ export default function About() {
         <div class="row">
           <div class="col-12 col-md-8">
             <p class="fs-lg text-gray-800 mb-6 mb-md-8">
-              Below are our Terms of Service, which outline a lot of legal
-              goodies, but the bottom line is it’s our aim to always take care
-              of both you, as a customer, or as a seller on our platform.
+              Dr Andrea Nanetti first theorised Engineering Historical Memory
+              (EHM) when he was Visiting Scholar at Princeton University in
+              2007. Since then, EHM is an ongoing research project that welcomes
+              international and multidisciplinary collaboration to design and
+              test interactive applications for virtual (re)organisation and
+              delivery of historical knowledge in the digital age. The aim is to
+              overcome linguistic obstacles and cultural barriers of historical
+              research in a transcultural (re)reading of primary sources and
+              secondary literature for the pre-modern history of the
+              Afro-Eurasian continent, its people and their interactions between
+              1205 and 1533. The stage being the intercontinental communication
+              networks by sea and by land as they were first identified by the
+              German geographer Ferdinand Freiherr von Richthofen (1833-1905) in
+              his magnum opus China (1877-1912). With equally and fairly
+              engagement of different cultural environments in Europe, Africa,
+              and Asia, this scholarly initiative also aims to contribute to the
+              forging of international, intercultural, and interreligious
+              dialogue and cooperation.
             </p>
-
-            <h3 class="mb-5">Licensing Terms</h3>
-
-            <p class="text-gray-800">
-              By visiting and/or taking any action on Bootstrap Themes, you
-              confirm that you are in agreement with and bound by the terms
-              outlined below. These terms apply to the website, emails, or any
-              other communication.
-            </p>
-
-            <p>Here are terms of our Standard License:</p>
-
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800">
-                The Standard License grants you a non-exclusive right to make
-                use of Theme you have purchased.
-              </p>
-            </div>
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800">
-                You are licensed to use the Item to create one End Product for
-                yourself or for one client (a “single application”), and the End
-                Product can be distributed for Free.
-              </p>
-            </div>
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800">
-                If a Theme contains code, images, or content sourced from
-                elsewhere under a different license, that item retains its
-                original license. The license for the code, images, or content
-                will be noted by the Theme author. You are responsible for
-                adhering to the original license or clearing it with the author
-                of the code, image, or content.
-              </p>
-            </div>
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800">
-                This license can be terminated if you breach it and you lose the
-                right to distribute the End Product until the Theme has been
-                fully removed from the End Product.
-              </p>
-            </div>
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800">
-                The author of the Theme retains ownership of the Theme, but
-                grants you the license on these terms. This license is between
-                the author of the Theme and you. Be Colossal LLC (Bootstrap
-                Themes) are not a party to this license or the one granting you
-                the license.
-              </p>
-            </div>
-
-            <p>If you opt for an Extended License:</p>
-
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800 mb-6 mb-md-8">
-                You are licensed to use the Item to create one End Product for
-                yourself or for one client (a “single application”), and the End
-                Product maybe sold or distributed for free.
-              </p>
-            </div>
-
-            <h3 class="mb-5">Additional Terms</h3>
-
-            <p class="text-gray-800">
-              By visiting and/or taking any action on Bootstrap Themes, you
-              confirm that you are in agreement with and bound by the terms
-              outlined below. These terms apply to the website, emails, or any
-              other communication.
-            </p>
-
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800">
-                All payments are processed securely through PayPal or Stripe.
-                Bootstrap Themes does not directly process payments through the
-                website.
-              </p>
-            </div>
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800">
-                You have 14 days to evaluate your purchase. If your purchase
-                fails to meet expectations set by the seller, or is critically
-                flawed in some way, contact Bootstrap Themes and we will issue a
-                full refund pending a review.
-              </p>
-            </div>
-            <div class="d-flex">
-              <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
-                <i class="fe fe-check"></i>
-              </span>
-
-              <p class="text-gray-800 mb-6 mb-md-0">
-                Membership is a benefit for those who follow our terms and
-                policies. We may at any time suspend or terminate your account.
-              </p>
-            </div>
           </div>
           <div class="col-12 col-md-4">
             <div class="card shadow-light-lg">
@@ -189,6 +76,138 @@ export default function About() {
                 </p>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="row">
+          <p class="fs-lg text-gray-800 mb-6 mb-md-8">
+            Since 2007, EHM is studying and practising "by what means"
+            traditional historical scholarship can supply machine-readable
+            information sets to empower historical sciences with artificial
+            intelligence and machine learning, thus enabling all users to read
+            primary historical sources according to different levels of
+            knowledge and expertise interactively. In the history domain, EHM
+            makes a cross-disciplinary use of established research processes,
+            such as mapping as understood in mathematics and linguistics (i.e.,
+            an operation that associates each element of a given set, the
+            domain, with one or more items of a second set, the range) and
+            parsing as understood in computing (i.e., analyse narratives into
+            logical syntactic components) to kick-off the exploration of primary
+            historical sources. Using these operations of mapping and parsing
+            for individual primary historical sources, EHM associates each
+            element of given sets of information provided by the domain of the
+            traditional disciplines (e.g., history, art history, philology,
+            palaeography, diplomatics, codicology, archaeology, epigraphy,
+            sigillography) with one or more elements of the range of
+            machine-readable content management systems (e.g., spreadsheets,
+            computational notebooks). The level of accuracy of this preliminary
+            human activity is directly proportional to that of the aggregations
+            generated and visualised by the EHM algorithms from different sets
+            of similar written or depicted elements in the EHM database (e.g.,
+            geographical names, people’s names, goods, ships, governments,
+            events, architectures, drawings) and from potentially relevant
+            publications, images, videos, and news retrieved in online
+            repositories.
+          </p>
+
+          <p class="fs-lg text-gray-800 mb-9 mb-md-11">
+            To understand and emphasise the unity of EHM as an interactive
+            system in the approach to historical information, the rhetorical and
+            conceptual linking of “mapping and visualisation” as well as “search
+            and visualisation” should be hendiadys. Thus, the term “mapping” is
+            understood, in its basic meaning, as the localization and
+            description of elements, facts or phenomena that relate to a
+            circumscribed area, historically understood at the intersection of
+            precise space and time coordinates. The aim is to provide and test
+            an example of an innovative epistemological process to visually
+            distil historical data. The “visualization” process is seen not as a
+            reductive representation to epitomise and/or illustrate written
+            narratives, but as an investigative tool that the historian
+            (especially one who intends to enter fully into the digital era) can
+            use for discovering and organising new relationships between
+            objects, in a new historical landscape where past, present, and
+            future can merge in a democratised whole (Altman, 2008; Bolick,
+            2006).
+          </p>
+
+          <p class="fs-lg text-gray-800mb-5">
+            The EHM approach to history can be construed as a hybrid
+            human-machine methodology because it relies on both human scholarly
+            touch and machine computational power. The attributes of the EHM
+            methodology (i.e., set of methods) can be named as follows.
+          </p>
+
+          <div class="d-flex">
+            <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
+              <i class="fe fe-check"></i>
+            </span>
+
+            <p class="text-gray-800">
+              Analytic, because of the scholarly mapping and parsing of
+              information from primary historical sources.
+            </p>
+          </div>
+          <div class="d-flex">
+            <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
+              <i class="fe fe-check"></i>
+            </span>
+
+            <p class="text-gray-800">
+              Synthetic, in reason of the interactive visualisation of selected
+              information.
+            </p>
+          </div>
+          <div class="d-flex">
+            <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
+              <i class="fe fe-check"></i>
+            </span>
+
+            <p class="text-gray-800">
+              Exploratory, because of the automatic search for online
+              publications, images, videos, and news potentially relevant to the
+              user’s choices.
+            </p>
+          </div>
+          <div class="d-flex">
+            <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
+              <i class="fe fe-check"></i>
+            </span>
+
+            <p class="text-gray-800">
+              Aggregative, as far as it allows interactive selections and
+              visualisations of different sets of search results.
+            </p>
+          </div>
+          <div class="d-flex mb-5">
+            <span class="badge badge-rounded-circle bg-success-soft mt-1 me-4">
+              <i class="fe fe-check"></i>
+            </span>
+
+            <p class="text-gray-800">
+              Non-narrative in principle, because the organisation of the
+              materials into narratives is up to the user who generates gamut
+              accordingly.
+            </p>
+          </div>
+          <div class="row">
+            <p class="fs-lg text-gray-800 mb-6 mb-md-8">
+              To understand and emphasise the unity of EHM as an interactive
+              system in the approach to historical information, the rhetorical
+              and conceptual linking of “mapping and visualisation” as well as
+              “search and visualisation” should be hendiadys. Thus, the term
+              “mapping” is understood, in its basic meaning, as the localization
+              and description of elements, facts or phenomena that relate to a
+              circumscribed area, historically understood at the intersection of
+              precise space and time coordinates. The aim is to provide and test
+              an example of an innovative epistemological process to visually
+              distil historical data. The “visualization” process is seen not as
+              a reductive representation to epitomise and/or illustrate written
+              narratives, but as an investigative tool that the historian
+              (especially one who intends to enter fully into the digital era)
+              can use for discovering and organising new relationships between
+              objects, in a new historical landscape where past, present, and
+              future can merge in a democratised whole (Altman, 2008; Bolick,
+              2006).
+            </p>
           </div>
         </div>
       </div>
