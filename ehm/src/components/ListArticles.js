da3df4604ebf1252c170ca9a76a1eac4b670322c
theo <theophilus96@gmail.com>
2021-06-06 02:04:22 +0800

    Update ListArticles.js

--- ehm/src/components/ListArticles.js
+++ ehm/src/components/ListArticles.js
@@ -10,7 +10,7 @@ export default function ListArticles(doc) {
   console.log("list category =>  id = ", CatId);
   console.log("list category => doc = ", doc);
   const { docs } = useFirestore("category/" + CatId + "/Article");
-  console.log("list category => ",docs);
+  console.log("list category => ", docs);
 
   // const [articleList, setarticleList] = useState("");
 
@@ -41,54 +41,57 @@ export default function ListArticles(doc) {
           <div className="col-12">
             {docs &&
               docs.map((doc) => (
-                <Link to={`/category/${CatId}/article/${doc.id}`}>
-                  <div
-                    className="card card-row shadow-light-lg mb-6 lift lift-lg"
-                    key={doc.id}
-                  >
-                    <div className="row gx-0">
-                      <div className="col-12">
-                        {/* <span className="badge rounded-pill bg-light badge-float badge-float-inside">
+                <div
+                  className="card card-row shadow-light-lg mb-6 lift lift-lg"
+                  key={doc.id}
+                >
+                  <div className="row gx-0">
+                    <div className="col-12">
+                      {/* <span className="badge rounded-pill bg-light badge-float badge-float-inside">
                           <span className="h6 text-uppercase">Featured</span>
                         </span> */}
+                    </div>
+
+                    <Link
+                      to={`/category/${CatId}/article/${doc.id}`}
+                      className="col-12 col-md-6 order-md-2 bg-cover card-img-end"
+                      style={{
+                        backgroundImage: `url(${doc.image})`,
+                      }}
+                    >
+                      <img
+                        src="assets/img/photos/photo-27.jpg"
+                        alt="..."
+                        className="img-fluid d-md-none invisible"
+                      />
+
+                      <div className="shape shape-start shape-fluid-y text-white d-none d-md-block">
+                        <svg
+                          viewBox="0 0 112 690"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M0 0h62.759v172C38.62 384 112 517 112 517v173H0V0z"
+                            fill="currentColor"
+                          />
+                        </svg>{" "}
                       </div>
-                      <a
-                        className="col-12 col-md-6 order-md-2 bg-cover card-img-end"
-                        style={{
-                          backgroundImage: `url(${doc.image})`,
-                        }}
-                        href="#!"
+                    </Link>
+                    <div className="col-12 col-md-6 order-md-1">
+                      <Link
+                        className="card-body"
+                        to={`/category/${CatId}/article/${doc.id}`}
                       >
-                        <img
-                          src="assets/img/photos/photo-27.jpg"
-                          alt="..."
-                          className="img-fluid d-md-none invisible"
-                        />
+                        <h3>{doc.name}</h3>
 
-                        <div className="shape shape-start shape-fluid-y text-white d-none d-md-block">
-                          <svg
-                            viewBox="0 0 112 690"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M0 0h62.759v172C38.62 384 112 517 112 517v173H0V0z"
-                              fill="currentColor"
-                            />
-                          </svg>{" "}
-                        </div>
-                      </a>
-                      <div className="col-12 col-md-6 order-md-1">
-                        <a className="card-body" href="#!">
-                          <h3>{doc.name}</h3>
+                        <p className="mb-0 text-muted">{doc.description}</p>
+                      </Link>
 
-                          <p className="mb-0 text-muted">{doc.description}</p>
-                        </a>
+                      <a className="card-meta" href="#!">
+                        <hr className="card-meta-divider" />
 
-                        <a className="card-meta" href="#!">
-                          <hr className="card-meta-divider" />
-
-                          {/* <div className="avatar avatar-sm me-2">
+                        {/* <div className="avatar avatar-sm me-2">
                             <img
                               src="assets/img/avatars/avatar-1.jpg"
                               alt="..."
@@ -96,18 +99,17 @@ export default function ListArticles(doc) {
                             />
                           </div> */}
 
-                          <h6 className="text-uppercase text-muted me-2 mb-0">
-                            {doc.citation}
-                          </h6>
+                        <h6 className="text-uppercase text-muted me-2 mb-0">
+                          {doc.citation}
+                        </h6>
 
-                          {/* <p className="h6 text-uppercase text-muted mb-0 ms-auto">
+                        {/* <p className="h6 text-uppercase text-muted mb-0 ms-auto">
                             <time datetime="2019-05-02">May 02</time>
                           </p> */}
-                        </a>
-                      </div>
+                      </a>
                     </div>
                   </div>
-                </Link>
+                </div>
               ))}
           </div>
         </div>
