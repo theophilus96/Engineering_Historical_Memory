e2fa777f9509289172846bd10368d665d3bde4e0
theo <theophilus96@gmail.com>
2021-06-18 00:45:35 +0800

    Update BottomList.js

--- ehm/src/components/BottomList.js
+++ ehm/src/components/BottomList.js
@@ -10,7 +10,7 @@ export default function BottomList() {
   // const { docs } = useFirestore("category");
   const { docs } = useSubcollect("category");
 
-  console.log("use sub collect ", docs);
+  // console.log("use sub collect ", docs);
 
   const MapsofAfroEurasia = useFirestore(
     "category/qujoO8JON704I5cm5WYn/Article"
@@ -23,40 +23,71 @@ export default function BottomList() {
   const documentID = [];
   //Also a good practice to separate reference instance
 
-  useEffect(() => {
-    const Catdocument = projectFirestore
-      .collection("category")
-      .onSnapshot((querySnapshot) => {
-        let allItems = [];
-        //querySnapshot is "iteratable" itself
-        querySnapshot.forEach((CatDoc) => {
-          //userDoc contains all metadata of Firestore object, such as reference and id
-          //   console.log("category ID", CatDoc.id);
-          projectFirestore
-            .collection("category")
-            .doc(CatDoc.id)
-            .collection("Article")
-            .get()
-            .then((querySnapshot) => {
-              //querySnapshot is "iteratable" itself
-              querySnapshot.forEach((ArtDoc) => {
-                //userDoc contains all metadata of Firestore object, such as reference and id
-                // console.dir(ArtDoc);
-                allItems.push({ ...ArtDoc.data(), id: ArtDoc.id });
-              });
-            });
-        });
-        setItems(allItems);
-      });
+  // useEffect(() => {
+  //   const Catdocument = projectFirestore
+  //     .collection("category")
+  //     .onSnapshot((querySnapshot) => {
+  //       let allItems = [];
+  //       //querySnapshot is "iteratable" itself
+  //       querySnapshot.forEach((CatDoc) => {
+  //         //userDoc contains all metadata of Firestore object, such as reference and id
+  //         //   console.log("category ID", CatDoc.id);
+  //         projectFirestore
+  //           .collection("category")
+  //           .doc(CatDoc.id)
+  //           .collection("Article")
+  //           .get()
+  //           .then((querySnapshot) => {
+  //             //querySnapshot is "iteratable" itself
+  //             querySnapshot.forEach((ArtDoc) => {
+  //               //userDoc contains all metadata of Firestore object, such as reference and id
+  //               // console.dir(ArtDoc);
+  //               allItems.push({ ...ArtDoc.data(), id: ArtDoc.id });
+  //             });
+  //           });
+  //       });
+  //       setItems(allItems);
+  //     });
 
-    return () => Catdocument();
-  }, []);
+  //   return () => Catdocument();
+  // }, []);
   //Get them
 
-  console.log("type", typeof docs);
-  console.log("items array", docs);
-  console.log("documentID[0] ", docs[0]);
-  console.log("type of documentID[0]", typeof docs[0]);
+  useEffect(() => {
+    (async () => {
+      await projectFirestore
+        .collection("category")
+        .get()
+        .then((querySnapshot) => {
+          let allItems = [];
+          querySnapshot.forEach((CatDoc) => {
+            console.log(CatDoc.id, " => ", CatDoc.data());
+            console.log(
+              CatDoc.id,
+              " => ",
+              projectFirestore
+                .collection("category")
+                .doc(CatDoc.id)
+                .collection("Article")
+                .get()
+                .then((querySnapshot) => {
+                  const data = querySnapshot.docs.map((ArtDoc) => {
+                    return ArtDoc.data();
+                  });
+                  console.log("data =>", data);
+                  allItems.push(data);
+                })
+            );
+          });
+          setItems(allItems);
+        });
+    })();
+  }, []);
+
+  console.log("type", typeof items);
+  console.log("items array", items);
+  console.log("documentID[0] ", items[0]);
+  console.log("type of documentID[0]", typeof items[0]);
 
   //   for (const x in documentID) {
   //     console.log("x", x);
@@ -77,10 +108,10 @@ export default function BottomList() {
   //   return element;
   // });
 
-  docs.map((doc) => {
-    console.log("docs ID in items", doc.id);
-    console.log("docs name in items", doc.name);
-  });
+  // docs.map((doc) => {
+  //   console.log("docs ID in items", doc.id);
+  //   console.log("docs name in items", doc.name);
+  // });
 
   items.map((doc) => {
     console.log("items ID in items", doc.id);
@@ -92,8 +123,8 @@ export default function BottomList() {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            {MapsofAfroEurasia &&
-              MapsofAfroEurasia.map((doc) => (
+            {docs &&
+              docs.map((doc) => (
                 <div
                   className="card card-row shadow-light-lg mb-6 lift lift-lg"
                   key={doc.id}
